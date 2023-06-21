36cec64aafdacedeffa227162bf2a826bcd537f6
Valentín Marchessi <valentin.marchessi@devsu.com>
2023-06-21 14:32:59 -0300

    Project init and config

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  testEnvironment: "jsdom",
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+  testPathIgnorePatterns: [
+    "<rootDir>[/\\\\](node_modules|.next)[/\\\\]",
+    "<rootDir>/__tests__/__mocks__",
+  ],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)": ["<rootDir>/src/$1"],
+    "^.+\\.(css|less|scss|sass)$": "<rootDir>/__tests__/__mocks__/styleMock.ts",
+  },
+};
+
+export default config;
