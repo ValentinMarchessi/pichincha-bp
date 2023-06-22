4a74956791dfad4e5159098dc31b465e7e96a738
Valentín Marchessi <valentin.marchessi@devsu.com>
2023-06-22 11:45:10 -0300

    Initial services tests

---
+++ src/services/index.test.ts
@@ -0,0 +1,10 @@
+import { AssetServices } from ".";
+
+describe("Asset Services", () => {
+  beforeEach(() => {
+    fetch.resetMock;
+  });
+  describe("getAssets", () => {
+    const assets = AssetServices.getAssets();
+  });
+});
