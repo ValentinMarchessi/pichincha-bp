8f4f35108b3ce604f53f1e4a940b5aeab0fe1423
Valentín Marchessi <valentin.marchessi@devsu.com>
2023-06-22 11:45:23 -0300

    Add header text to AssetForm

--- src/pages/AssetForm.tsx
+++ src/pages/AssetForm.tsx
@@ -1,8 +1,11 @@
 export default function AssetForm() {
   return (
-    <form>
-      <input type="text" placeholder="Asset Name" />
-      <input type="text" placeholder="Asset Description" />
-    </form>
+    <div>
+      <h1>Formulario de Registro</h1>
+      <form>
+        <input type="text" placeholder="Asset Name" />
+        <input type="text" placeholder="Asset Description" />
+      </form>
+    </div>
   );
 }
