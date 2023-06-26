291d5c5f8d3f18d45a8bae7538b7f7f4ba650aa9
Valentín Marchessi <valentin.marchessi@devsu.com>
2023-06-26 12:35:09 -0300

    Add useInput validator tests

--- src/lib/hooks/useInput.test.ts
+++ src/lib/hooks/useInput.test.ts
@@ -15,13 +15,97 @@ describe("useInput", () => {
     disabled: false,
     label: "label",
   };
-  it("Takes a name and options as arguments", () => {
-    const { result } = renderHook(() => useInput(name, options));
-    expect(result.current).toBeDefined();
-  });
   it("Defaults to a text input", () => {
     const { result } = renderHook(() => useInput(name));
     expect(result.current.props.type).toBe("text");
+  });
+  describe("Arguments", () => {
+    it("Takes a name and options as arguments", () => {
+      const { result } = renderHook(() => useInput(name, options));
+      expect(result.current).toBeDefined();
+    });
+    describe("Options", () => {
+      describe("validator", () => {
+        it("Is called when value changes", async () => {
+          const validator = jest.fn(() => "error message");
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          await waitFor(() => expect(validator).toBeCalledWith("new value"));
+        });
+        it("Can be async", () => {
+          const validator = jest.fn(() => Promise.resolve("error message"));
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          waitFor(() => expect(validator).resolves.toBe("error message"));
+        });
+        it("Sets the error message if it returns a string", () => {
+          const validator = jest.fn(() => "error message");
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          waitFor(() => expect(result.current.error).toBe("error message"));
+        });
+        it("Sets the error message if it returns a promise", () => {
+          const validator = jest.fn(() => Promise.resolve("error message"));
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          waitFor(() => expect(result.current.error).toBe("error message"));
+        });
+        it("Sets the error message as an empty string if it returns null", () => {
+          const validator = jest.fn(() => null);
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          waitFor(() => expect(result.current.error).toBe(""));
+        });
+        it("Sets the error message as an empty string if it resolves null", () => {
+          const validator = jest.fn(() => Promise.resolve(null));
+          const { result } = renderHook(() =>
+            useInput(name, { ...options, validator })
+          );
+          act(() => {
+            result.current.setValue("new value");
+            result.current.props.onBlur?.(
+              {} as unknown as React.FocusEvent<HTMLInputElement, Element>
+            );
+          });
+          waitFor(() => expect(result.current.error).toBe(""));
+        });
+      });
+    });
   });
   describe("Return object", () => {
     describe("label", () => {
@@ -83,6 +167,36 @@ describe("useInput", () => {
           result.current.setValue("new value");
         });
         waitFor(() => expect(result.current.props.value).toBe("new value"));
+      });
+    });
+    describe("reset", () => {
+      it("Resets the value to the default value", async () => {
+        const { result } = renderHook(() => useInput(name, options));
+        act(() => {
+          result.current.setValue("new value");
+        });
+        await waitFor(() =>
+          expect(result.current.props.value).toBe("new value")
+        );
+        act(() => {
+          result.current.reset();
+        });
+        await waitFor(() =>
+          expect(result.current.props.value).toBe(options.defaultValue)
+        );
+      });
+      it("Resets the value to an empty string if no default value is provided", async () => {
+        const { result } = renderHook(() => useInput(name));
+        act(() => {
+          result.current.setValue("new value");
+        });
+        await waitFor(() =>
+          expect(result.current.props.value).toBe("new value")
+        );
+        act(() => {
+          result.current.reset();
+        });
+        await waitFor(() => expect(result.current.props.value).toBe(""));
       });
     });
   });
